edfc7bd949ba34321b674d9d611a84601d805f59
无名 无姓 <lsj2543075618@gmail.com>
2023-04-02 00:09:31 +0800

    添加Gtest内容

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "addDLL.h"
 #include "minusDLL.h"
+#include <gtest/gtest.h>
 
-int main()
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+
+int main(int argc, char** argv) 
 {
     int addResult = add(2,3);
     int minusResult = minus(5,2);
@@ -11,5 +17,8 @@ int main()
     std::cout << "add(2,3) == " << addResult << std::endl;
     std::cout << "minus(5,2) == " << minusResult << std::endl;
     std::cout << "minus(5.5f, 2.2f) " << minusResult2 << std::endl;
-    return 0;
+    
+
+    ::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
