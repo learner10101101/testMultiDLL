8127f627dd7f88bd2e5fa34e0d98386f0deb46a3
无名 无姓 <lsj2543075618@gmail.com>
2023-04-10 16:05:08 +0800

    Jenkins的WORKSPACE路径可以直接替换到工作区的项目目录

--- jenkins_build.bat
+++ jenkins_build.bat
@@ -1,11 +1,11 @@
 echo ======================start build======================
 echo startTime: %date:~0,4%/%date:~5,2%/%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2% 
-cd %WORKSPACE%\test 
+cd %WORKSPACE% 
 rd /s /q build 
 mkdir build 
 cd build 
 cmake .. -G "Visual Studio 16 2019" -A x64
-devenv.com %WORKSPACE%\test\build\testMultiDLL.sln /Build "Debug|x64" > %WORKSPACE%\test\VS_build.log
-call %WORKSPACE%\test\bin\Debug\testMultiDLL.exe 
+devenv.com %WORKSPACE%\build\testMultiDLL.sln /Build "Debug|x64" > %WORKSPACE%\VS_build.log
+call %WORKSPACE%\bin\Debug\testMultiDLL.exe 
 echo endTime: %date:~0,4%/%date:~5,2%/%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2%
 echo ======================end build======================
